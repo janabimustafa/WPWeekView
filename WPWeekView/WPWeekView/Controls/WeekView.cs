aa4e5c8c4beb20cc61f038f1b23415b47efe68a4
janabimustafa <mustafa@mustafataleb.com>
2014-10-22 03:04:27 -0500

    Started with WeekView.cs

---
+++ WPWeekView/WPWeekView/Controls/WeekView.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WPWeekView.Controls
+{
+    class WeekView : Control
+    {
+        /// <summary>
+        /// Gets or sets the starting day of the week to be dispalyed
+        /// </summary>
+        public DayOfWeek StartingDay { get; set; }
+
+        public WeekView()
+        {            
+            DefaultStyleKey = typeof(WeekView);
+        }
+
+        /// <summary>
+        /// Sets the days of the week to display in the <see cref="WeeView">WeekView</see> Calendar control to the desired <see cref="DayOfWeek">DayOfWeek(s)</see>.
+        /// </summary>
+        /// <example>
+        /// This example shows how to set the days of the week of this control to Monday and Sunday
+        /// <code>
+        /// var view = new WeekView();
+        /// view.SetDaysOfWeek(DayOfWeek.Monday | DayOfWeek.Sunday);
+        /// </code>
+        /// </example>
+        /// <param name="days"></param>
+        public void SetDaysOfWeek(params DayOfWeek days)
+        {
+
+        }
+
+
+    }
+}
